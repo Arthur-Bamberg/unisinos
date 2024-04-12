50013fb03e39546ee84279ebf01af5e1bc8ce010
“Arthur-Bamberg” <bamberguisses@gmail.com>
2024-04-12 13:08:41 -0300

    feat: começo da classe Loja

---
+++ prog1/desafio/ArthurBambergGR96002-00592/src/Loja.java
@@ -0,0 +1,51 @@
+public class Loja {
+    private String nome;
+    private int quantidadeFuncionarios;
+    private float salarioBaseFuncionario;
+
+    public Loja(String nome, int quantidadeFuncionarios, float salarioBaseFuncionario) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    public Loja(String nome, int quantidadeFuncionarios) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = -1;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadeFuncionarios() {
+        return quantidadeFuncionarios;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    public float getSalarioBaseFuncionario() {
+        return salarioBaseFuncionario;
+    }
+
+    public void setSalarioBaseFuncionario(float salarioBaseFuncionario) {
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    @Override
+    public String toString() {
+        return "Loja [nome=" + nome + ", quantidadeFuncionarios=" + quantidadeFuncionarios + ", salarioBaseFuncionario="
+                + salarioBaseFuncionario + ", getNome()=" + getNome() + ", getQuantidadeFuncionarios()="
+                + getQuantidadeFuncionarios() + ", getSalarioBaseFuncionario()=" + getSalarioBaseFuncionario()
+                + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+                + "]";
+    }
+
+}
